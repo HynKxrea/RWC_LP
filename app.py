041246356e8e53bf0d65f8b707487b8f81f88af5
HyunJun Park <phj010217@naver.com>
2024-12-21 19:03:37 +0900

    streamlit code v1

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+st.title("RWC: LP Solver")
+st.write("""
+         This model is made for students who study 'Linear Programming'. 
+        We can solve all kinds of LP problems like feasible, infeasible, unbounded and so on. 
+        I'll give you a simple guideline to help your better use.
+
+        1. Upload your LP problem that you cannot solve.
+        2. Check the formulation and compare with yours.
+        3. Modify your answer and practice. 
+        """)
